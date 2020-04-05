f95350f3b137d7f8f366deac075050ddb0a4d88e
bandiang2 <barmand@aims.ac.rw>
2020-04-05 10:53:34 +0000

    Day 5 + solution

--- Day5.py
+++ Day5.py
@@ -29,3 +29,11 @@
 
 #### SOLULTION 
 
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        som = 0
+        for i in range(len(prices)-1):
+            if (prices[i+1] - prices[i]) > 0:
+                som += (prices[i+1] - prices[i])
+        return som
+
