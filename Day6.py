9a79ff753c6dc93ed6bbc8329faf998e8de8dd03
bandiang2 <barmand@aims.ac.rw>
2020-04-06 10:07:06 +0000

    Day 6: Group Anagrams

--- Day6.py
+++ Day6.py
@@ -19,4 +19,19 @@
 #	All inputs will be in lowercase.
 #	The order of your output does not matter.
 
+### My Solution 
 
+class Solution(object):
+    def groupAnagrams(self, strs):
+        """
+        :type strs: List[str]
+        :rtype: List[List[str]]
+        """
+        anagrams = {}
+        for s in strs:
+            inter_s = tuple(sorted(s))
+            if inter_s in anagrams:
+                anagrams[inter_s].append(s)
+            else:
+                anagrams[inter_s] = [s]
+        return anagrams.values()
