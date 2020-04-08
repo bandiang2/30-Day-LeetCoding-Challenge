add67a3403f66084421a500d00b73f953c35885c
bandiang2 <barmand@aims.ac.rw>
2020-04-08 10:50:36 +0000

    Day 8: Middle of the Linked list

--- Day8.py
+++ Day8.py
@@ -24,3 +24,21 @@
 
 # My Solution
 
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution(object):
+    def middleNode(self, head):
+        """
+        :type head: ListNode
+        :rtype: ListNode
+        """
+        p1, p2 = head, head
+        while p2 and p2.next:
+            p1 = p1.next
+            p2 = p2.next.next
+        return p1
+        
